253f57a981f6e4ee4c65c28074fd936abc4e369d
hoijui <hoijui.quaero@gmail.com>
2021-10-15 12:24:11 +0200

    sources::github_ci: Moves functionality into functions

--- src/sources/github_ci.rs
+++ src/sources/github_ci.rs
@@ -41,6 +41,67 @@ fn is_tag(environment: &mut Environment, refr: &str) -> BoxResult<Option<String>
     Ok(tag.map(std::borrow::ToOwned::to_owned))
 }
 
+fn build_branch(environment: &mut Environment) -> BoxResult<Option<String>> {
+    let refr = var(environment, "GITHUB_REF");
+    Ok(if let Some(refr) = refr {
+        is_branch(environment, &refr)?
+    } else {
+        None
+    })
+}
+
+fn build_tag(environment: &mut Environment) -> BoxResult<Option<String>> {
+    let refr = var(environment, "GITHUB_REF");
+    Ok(if let Some(refr) = refr {
+        is_tag(environment, &refr)?
+    } else {
+        None
+    })
+}
+
+fn repo_clone_url(
+    source: &dyn super::VarSource,
+    environment: &mut Environment,
+) -> BoxResult<Option<String>> {
+    let repo_web_url = source.retrieve(environment, Key::RepoWebUrl)?;
+    Ok(if let Some(repo_web_url) = repo_web_url {
+        // usually:
+        // https://github.com/hoijui/nim-ci.git
+        // https://gitlab.com/hoijui/tebe.git
+        Some(tools::git::web_to_clone_url(&repo_web_url, false)?)
+    } else {
+        None
+    })
+}
+
+fn repo_web_url(environment: &mut Environment) -> Option<String> {
+    match (
+        environment.vars.get("GITHUB_SERVER_URL"),
+        environment.vars.get("GITHUB_REPOSITORY"),
+    ) {
+        (Some(server), Some(repo)) => {
+            // "${GITHUB_SERVER_URL}/${GITHUB_REPOSITORY}"
+            // usually:
+            // GITHUB_SERVER_URL="https://github.com/"
+            // GITHUB_REPOSITORY="user/project"
+            Some(format!("{}/{}", server, repo))
+        }
+        (_, _) => None,
+    }
+}
+
+fn build_hosting_url(
+    source: &dyn super::VarSource,
+    environment: &mut Environment,
+) -> BoxResult<Option<String>> {
+    let repo_web_url = source.retrieve(environment, Key::RepoWebUrl)?;
+    Ok(if let Some(repo_web_url) = repo_web_url {
+        Some(tools::git::web_to_build_hosting_url(&repo_web_url)?) // TODO This currently comes without final '/', is that OK?
+    } else {
+        None
+    })
+}
+
 impl super::VarSource for VarSource {
     fn is_usable(&self, _environment: &mut Environment) -> bool {
         true
@@ -64,49 +125,11 @@ impl super::VarSource for VarSource {
             Key::NameMachineReadable => {
                 super::try_construct_machine_readable_name_from_web_url(self, environment)?
             }
-            Key::RepoWebUrl => {
-                match (
-                    environment.vars.get("GITHUB_SERVER_URL"),
-                    environment.vars.get("GITHUB_REPOSITORY"),
-                ) {
-                    (Some(server), Some(repo)) => {
-                        // "${GITHUB_SERVER_URL}/${GITHUB_REPOSITORY}"
-                        // usually:
-                        // GITHUB_SERVER_URL="https://github.com/"
-                        // GITHUB_REPOSITORY="user/project"
-                        Some(format!("{}/{}", server, repo))
-                    }
-                    (_, _) => None,
-                }
-            }
+            Key::RepoWebUrl => repo_web_url(environment),
             Key::Ci => var(environment, "CI"),
-            Key::BuildBranch => {
-                let refr = var(environment, "GITHUB_REF");
-                if let Some(refr) = refr {
-                    is_branch(environment, &refr)?
-                } else {
-                    None
-                }
-            }
-            Key::BuildTag => {
-                let refr = var(environment, "GITHUB_REF");
-                if let Some(refr) = refr {
-                    is_tag(environment, &refr)?
-                } else {
-                    None
-                }
-            }
-            Key::RepoCloneUrl => {
-                let repo_web_url = self.retrieve(environment, Key::RepoWebUrl)?;
-                if let Some(repo_web_url) = repo_web_url {
-                    // usually:
-                    // https://github.com/hoijui/nim-ci.git
-                    // https://gitlab.com/hoijui/tebe.git
-                    Some(tools::git::web_to_clone_url(&repo_web_url, false)?)
-                } else {
-                    None
-                }
-            }
+            Key::BuildBranch => build_branch(environment)?,
+            Key::BuildTag => build_tag(environment)?,
+            Key::RepoCloneUrl => repo_clone_url(self, environment)?,
             Key::RepoRawVersionedPrefixUrl => {
                 super::try_construct_raw_prefix_url(self, environment)?
             }
@@ -117,14 +140,7 @@ impl super::VarSource for VarSource {
                 super::try_construct_dir_prefix_url(self, environment)?
             }
             Key::RepoCommitPrefixUrl => super::try_construct_commit_prefix_url(self, environment)?,
-            Key::BuildHostingUrl => {
-                let repo_web_url = self.retrieve(environment, Key::RepoWebUrl)?;
-                if let Some(repo_web_url) = repo_web_url {
-                    Some(tools::git::web_to_build_hosting_url(&repo_web_url)?) // TODO This currently comes without final '/', is that OK?
-                } else {
-                    None
-                }
-            }
+            Key::BuildHostingUrl => build_hosting_url(self, environment)?,
             Key::RepoIssuesUrl => super::try_construct_issues_url(self, environment)?,
             Key::BuildOs => var(environment, "RUNNER_OS"), // TODO Not sure if this makes sense ... have to check in practise!
             Key::Version => var(environment, "GITHUB_SHA"),
