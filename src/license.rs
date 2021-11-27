54f2bfeb5f32eb1da6e219715a97250be5979ef5
hoijui <hoijui.quaero@gmail.com>
2021-11-28 00:32:24 +0100

    Simplifies and improves license checking

--- src/license.rs
+++ src/license.rs
@@ -4,65 +4,44 @@
 
 use std::fmt;
 
-/// An owned/no-lifetimes transcription of [`spdx::error::ParseError`].
-#[derive(thiserror::Error, Debug)]
-pub struct SpdxParseError {
-    /// The string that was attempting to be parsed
-    pub original: String,
-    /// The range of characters in the original string that result
-    /// in this error
-    pub span: std::ops::Range<usize>,
-    // /// The specific reason for the error
-    // pub reason: spdx::error::Reason, // TODO Can't use this, as Reason does not impl Clone
-}
-
-impl From<spdx::error::ParseError<'_>> for SpdxParseError {
-    fn from(err: spdx::error::ParseError<'_>) -> Self {
-        SpdxParseError {
-            original: err.original.to_owned(),
-            span: err.span,
-            // reason: err.reason,
-        }
-    }
-}
-
-impl<'a> From<&'a SpdxParseError> for spdx::error::ParseError<'a> {
-    fn from(err: &'a SpdxParseError) -> Self {
-        spdx::error::ParseError {
-            original: &err.original,
-            span: err.span.clone(),
-            reason: spdx::error::Reason::Empty,
-        }
-    }
-}
-
-impl fmt::Display for SpdxParseError {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        let err: spdx::error::ParseError = self.into();
-        err.fmt(f)
-    }
-}
-
 /// An owned/no-lifetimes transcription of `Vec<&spdx::expression::ExpressionReq>`
-#[derive(thiserror::Error, Debug)]
-pub struct ApprovementFailure {
-    /// The list of expresions that were not approved
+#[derive(Debug, Clone)]
+pub struct EvaluationError {
+    // The original expression that the ranges of the expressions reffer to
+    pub expression: String,
+    /// The list of expressions that failed
     pub failed: Vec<spdx::expression::ExpressionReq>,
 }
 
-impl From<Vec<&spdx::expression::ExpressionReq>> for ApprovementFailure {
-    fn from(err: Vec<&spdx::expression::ExpressionReq>) -> Self {
+impl From<(String, Vec<&spdx::expression::ExpressionReq>)> for EvaluationError {
+    fn from((expression, failures): (String, Vec<&spdx::expression::ExpressionReq>)) -> Self {
         Self {
-            failed: err.iter().map(|req| req.to_owned().clone()).collect(),
+            expression,
+            failed: failures.iter().map(|req| req.to_owned().clone()).collect(),
         }
     }
 }
 
-impl fmt::Display for ApprovementFailure {
+impl fmt::Display for EvaluationError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        f.write_fmt(format_args!("{:?}", self.failed))
+        f.write_fmt(format_args!(
+            "evaluation failure(s) in SPDX expression \"{}\": [",
+            self.expression
+        ))?;
+        for req in &self.failed {
+            // f.write_fmt(format_args!("    Failed '{}' at \"{}\"", req.req, self.expression[(req.span.start)..(req.span.end)]))?;
+            let expr_part = &self.expression[(req.span.start as usize)..(req.span.end as usize)];
+            f.write_fmt(format_args!(
+                "{{ '{}' - @({},{}) - \"{}\" }}, ",
+                req.req, req.span.start, req.span.end, expr_part
+            ))?;
+        }
+        f.write_str("]")?;
+        Ok(())
     }
 }
+
+impl std::error::Error for EvaluationError {}
 
 #[derive(thiserror::Error, Debug)]
 pub enum Error {
@@ -70,24 +49,25 @@ pub enum Error {
     NoLicense,
 
     #[error("The license expression is not in a valid SPDX format; see <>.")]
-    ParsingFailed(#[from] SpdxParseError),
+    ParsingFailed(#[from] spdx::ParseError),
 
     #[error("The license specifier is valid, but the licensing scheme is not approved.")]
-    NotApproved(#[from] ApprovementFailure),
+    NotApproved(#[from] EvaluationError),
 }
 
 pub fn validate_spdx_expr(expr: &str) -> Result<(), Error> {
     if expr.is_empty() {
         return Err(Error::NoLicense);
     }
-    let spdx_expr = spdx::Expression::parse(expr).map_err(SpdxParseError::from)?;
+    let spdx_expr = spdx::Expression::parse(expr)?;
     spdx_expr
+        // .evaluate_with_failures(|req| {
         .evaluate_with_failures(|req| {
             if let spdx::LicenseItem::Spdx { id, .. } = req.license {
                 return id.is_osi_approved();
             }
             false
         })
-        .map_err(ApprovementFailure::from)?;
+        .map_err(|failures| EvaluationError::from((expr.to_owned(), failures)))?;
     Ok(())
 }
