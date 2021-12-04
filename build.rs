949abe995f0a1a57f011e0d62953dd1428e03f06
hoijui <hoijui.quaero@gmail.com>
2021-12-04 17:43:39 +0100

    Use crate askalondo to identify LICENSE files

---
+++ build.rs
@@ -0,0 +1,27 @@
+// SPDX-FileCopyrightText: 2021 Robin Vobruba <hoijui.quaero@gmail.com>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+use std::env;
+use std::fs;
+use std::path::Path;
+
+const LICENSES_CACHE_FILE: &str = "resources/licenses-cache.bin.zstd";
+const LICENSES_CACHE_URL: &str =
+    "https://github.com/o2sh/onefetch/raw/main/resources/licenses/cache.bin.zstd";
+
+fn download_licenses_cache() -> Result<(), Box<dyn std::error::Error>> {
+    let cache_file = Path::new(&env::var("OUT_DIR")?).join(LICENSES_CACHE_FILE);
+    if !cache_file.exists() {
+        fs::create_dir_all(cache_file.parent().unwrap())?;
+        let url = reqwest::Url::parse(LICENSES_CACHE_URL)?;
+        let content = reqwest::blocking::get(url)?.bytes()?;
+        fs::write(cache_file, content)?;
+    }
+    println!("cargo:rerun-if-env-changed=OUT_DIR");
+    Ok(())
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    download_licenses_cache()
+}
--- src/license.rs
+++ src/license.rs
@@ -4,6 +4,18 @@
 
 use std::fmt;
 
+use askalono::{Store, TextData};
+use clap::lazy_static::lazy_static;
+use std::{ffi::OsStr, fs};
+
+const LICENSE_FILE_PREFIXES: [&str; 3] = ["LICENSE", "LICENCE", "COPYING"];
+
+static CACHE_DATA: &[u8] = include_bytes!(concat!(
+    env!("OUT_DIR"),
+    "/resources/licenses-cache.bin.zstd"
+));
+const MIN_THRESHOLD: f32 = 0.8;
+
 /// An owned/no-lifetimes transcription of `Vec<&spdx::expression::ExpressionReq>`
 #[derive(Debug, Clone)]
 pub struct EvaluationError {
@@ -70,4 +82,69 @@ pub fn validate_spdx_expr(expr: &str) -> Result<(), Error> {
         })
         .map_err(|failures| EvaluationError::from((expr.to_owned(), failures)))?;
     Ok(())
+}
+
+pub fn get_licenses(dir: &str) -> Result<Vec<String>, std::io::Error> {
+    lazy_static! {
+        static ref DIR_LICENSES_EXTRACTOR: Detector = Detector::new();
+    }
+
+    DIR_LICENSES_EXTRACTOR.get_licenses(dir)
+}
+
+/// A basic wrapper around the askalono library;
+/// originally from here:
+/// <https://github.com/o2sh/onefetch/blob/main/src/info/license.rs>
+/// (MIT licensed)
+struct Detector {
+    store: Store,
+}
+
+impl Detector {
+    pub fn new() -> Self {
+        Self {
+            store: Store::from_cache(CACHE_DATA).unwrap(),
+        }
+    }
+
+    /// Returns a list of SPDX licnese identifiers;
+    /// one for each LICENSE file found in the given directory.
+    pub fn get_licenses(&self, dir: &str) -> Result<Vec<String>, std::io::Error> {
+        fn is_license_file<S: AsRef<str>>(file_name: S) -> bool {
+            LICENSE_FILE_PREFIXES
+                .iter()
+                .any(|&lf_prefix| file_name.as_ref().starts_with(lf_prefix))
+        }
+
+        let mut output = fs::read_dir(dir)?
+            .filter_map(std::result::Result::ok)
+            .map(|entry| entry.path())
+            .filter(|entry| {
+                entry.is_file()
+                    && entry
+                        .file_name()
+                        .map(OsStr::to_string_lossy)
+                        .map(is_license_file)
+                        .unwrap_or_default()
+            })
+            .filter_map(|entry| {
+                let contents = fs::read_to_string(entry).unwrap_or_default(); // TODO Not too clean; we should possibly fail the function instead of silently skipping the file on error
+                self.analyze(&contents)
+            })
+            .collect::<Vec<_>>();
+
+        output.sort();
+        output.dedup();
+        Ok(output)
+    }
+
+    fn analyze(&self, text: &str) -> Option<String> {
+        let matched = self.store.analyze(&TextData::from(text));
+
+        if matched.score >= MIN_THRESHOLD {
+            Some(matched.name.into())
+        } else {
+            None
+        }
+    }
 }
